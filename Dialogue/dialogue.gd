686001e5dff3e0012529f513f2e78fdaedd61c2d
Christine Doan <angelstudyblr@gmail.com>
2024-03-19 16:48:36 -0700

    Setting up project, working on setting up the basics of the chat messaging mechanic

---
+++ Dialogue/dialogue.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+export(String, FILE, "*.json") var chatFile
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
