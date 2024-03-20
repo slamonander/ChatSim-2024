80a706e74bc7aeaeba87d4a36a4dad3a21dbe3f0
Christine Doan <angelstudyblr@gmail.com>
2024-03-20 01:00:31 -0700

    Enabled the Dialogue Manager add-on and continued tweaking the chat.json file

--- Dialogue/dialogue.gd
+++ Dialogue/dialogue.gd
@@ -1,7 +1,32 @@
 extends CanvasLayer
 
-export(String, FILE, "*.json") var chatFile
+@export_file("*.json") var chatFile
+
+var currentDialogueID = 0
+var dialogue = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start()
+	
+func start():
+	dialogue = load_dialogue()
+	currentDialogueID = -1
+	next_script()
+	
+
+func load_dialogue():
+	var file = FileAccess.open("res://Dialogue/json/chat.json", FileAccess.READ)
+	var content = JSON.parse_string(file.get_as_text())
+	return content
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		next_script()
+	
+func next_script():
+	currentDialogueID += 1
+	if currentDialogueID >= len(dialogue):
+		return
+		
+	$NinePatchRect/Chat.text = dialogue[currentDialogueID]['text']
