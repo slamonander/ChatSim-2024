b80f87031da3f697930a1dcf14dab27d3ea349f0
Christine Doan <angelstudyblr@gmail.com>
2024-03-20 23:08:24 -0700

    Trying to mess around with Dialogue Manager and customizing the text bubble to work with Dialogue Manager

---
+++ Dialogue/chat.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+var resource = load("res://Dialogue/chat.dialogue")
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+
+
